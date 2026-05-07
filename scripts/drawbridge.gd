6ef7467b16141bcdd904f82b4ab423be6165ce6f
pagliaria <pagliaria@gmail.com>
2026-05-06 22:27:23 -0400

    added barrier outside for battle

--- scripts/drawbridge.gd
+++ scripts/drawbridge.gd
@@ -9,6 +9,7 @@ extends Node2D
 @onready var bridge_up         : Sprite2D          = $bridge_up
 @onready var anim_player       : AnimationPlayer   = $AnimationPlayer
 @onready var bridge_collision  : CollisionShape2D  = $Wall_Collision/bridge_collision
+@onready var battle_seperator  : CollisionShape2D  = $Wall_Collision/BattleSeperator
 
 var _is_raised := false
 
@@ -18,19 +19,6 @@ func _ready() -> void:
 	bridge_down.visible    = true
 	bridge_up.visible      = true
 	bridge_collision.disabled = true
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and not event.echo:
-		if event.keycode == KEY_B:
-			_toggle()
-
-func _toggle() -> void:
-	if anim_player.is_playing():
-		return
-	if _is_raised:
-		_do_lower()
-	else:
-		_do_raise()
 
 func force_raise() -> void:
 	if _is_raised:
@@ -45,9 +33,11 @@ func force_lower() -> void:
 func _do_raise() -> void:
 	anim_player.play("raise")
 	bridge_collision.disabled = false
+	battle_seperator.disabled = true
 	_is_raised = true
 
 func _do_lower() -> void:
 	anim_player.play("lower")
 	bridge_collision.disabled = true
+	battle_seperator.disabled = false
 	_is_raised = false
