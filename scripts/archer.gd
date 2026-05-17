fdfc6ba8cdf207139ea17ac27935213dff9643f7
pagliaria <pagliaria@gmail.com>
2026-05-17 14:50:15 -0400

    changed attack rate for archers to not be animation litmited

--- scripts/archer.gd
+++ scripts/archer.gd
@@ -50,7 +50,7 @@ func _get_attack_damage() -> int:
 	return BASE_ATTACK_DAMAGE + _level_damage_bonus + get_building_attack_damage_bonus()
 
 func _get_attack_rate() -> float:
-	return maxf(0.5, BASE_ATTACK_RATE + _level_attack_rate_bonus) * get_building_attack_speed_multiplier()
+	return maxf(0.1, BASE_ATTACK_RATE + _level_attack_rate_bonus) * get_building_attack_speed_multiplier()
 
 func _get_attack_range() -> float:
 	return SHOOT_RANGE + get_building_range_bonus()
@@ -174,7 +174,11 @@ func _do_shoot() -> void:
 	var frames   : int   = _sprite.sprite_frames.get_frame_count("shoot")
 	var fps      : float = _sprite.sprite_frames.get_animation_speed("shoot")
 	var anim_dur : float = frames / fps
-	_sprite.speed_scale = maxf(1.0, anim_dur / _get_attack_rate())
+	print("anim_dur ", anim_dur)
+	print("attack rate ", _get_attack_rate())
+	print("ratio ",anim_dur / _get_attack_rate())
+	_sprite.speed_scale = maxf(1, anim_dur / _get_attack_rate())
+	print("speed scale ", _sprite.speed_scale)
 	_sprite.play("shoot")
 	var half_dur : float = (anim_dur / _sprite.speed_scale) * 0.5
 	await get_tree().create_timer(half_dur).timeout
@@ -187,10 +191,9 @@ func _spawn_arrow() -> void:
 		var arrow := ARROW_SCENE.instantiate()
 		get_parent().add_child(arrow)
 		arrow.global_position = global_position
-		arrow.init(_target, _get_attack_damage(), self)   # pass self for XP
-	_shooting     = false
-	_attack_timer = _get_attack_rate()
-	await _sprite.animation_finished
+		arrow.init(_target, _get_attack_damage(), self)
+	_shooting           = false
+	_attack_timer       = _get_attack_rate()
 	_sprite.speed_scale = 1.0
 	_sprite.play("idle")
 
